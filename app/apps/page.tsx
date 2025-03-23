169aa3fd8a98146b4f436175bd485444aac83d19
Fuuin404 <agiapal@example.com>
2025-03-23 15:56:30 +0000

    removed apostrophe

--- app/apps/page.tsx
+++ app/apps/page.tsx
@@ -3,7 +3,7 @@ export default function Apps() {
     <div className="mb-4">
       <h1 className="text-3xl font-semibold">Apps</h1>
       <p>This is the apps page.</p>
-      <p>This is a static page, so it doesn't fetch data from a server.</p>
+      <p>This is a static page</p>
     </div>
   );
 }
