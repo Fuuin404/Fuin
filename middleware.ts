c0dfb63b82c809d5b19c52b6071c07e5a4ce27a0
Fuuin404 <agiapal@example.com>
2025-03-26 20:29:30 +0000

    fixed login state mismatch

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,6 @@ export default withAuth(async function middleware() {}, {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|api/auth|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
